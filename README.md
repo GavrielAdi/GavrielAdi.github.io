4865da16bfb18b7e34e9d2ae5723abc56868dcde
unknown <gavriel.k.adi12@gmail.com>
2023-11-01 10:47:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#vue-calculator
+Gavriel Pramuda Kurniaadi
+5027221031
+```
+npm install
+```
+```
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Vue Calculator</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
